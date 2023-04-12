f937eddcb586102779dfff42ad23ae413955e25a
20201tiimi0047 <sallescardosodavi@gmail.com>
2023-04-12 10:10:55 -0300

    em andamento com novas telinhas códigos a mais

--- app/src/main/java/salles/cardoso/davi/galeria/activity/NewItemActivity.java
+++ app/src/main/java/salles/cardoso/davi/galeria/activity/NewItemActivity.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -41,34 +42,58 @@ public class NewItemActivity extends AppCompatActivity {
                 startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
             }
 
-            Button btnAddItem = findViewById(R.id.btnAddItem);
+        });
+        Button btnAddItem = findViewById(R.id.btnAddItem);
 
-            btnAddItem.setOnClickListener(new View.OnclickListener()){
-                public void onClick(View v){
+        btnAddItem.setOnClickListener(new View.OnclickListener()){
+            @Override
+            public void onClick(View v){
 
-                    if (photoSelected == null) {
-                        Toast.makeText(NewItemActivity.this, "É necessário selecionar uma imagem!", Toast.LENGTH_LONG).show();
-                        return;
-                    }
+                if (photoSelected == null) {
+                    Toast.makeText(NewItemActivity.this, "É necessário selecionar uma imagem!", Toast.LENGTH_LONG).show();
+                    return;
                 }
-            }
-            protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
-                //chamando a superclasse de onActivity com 3 parâmetros (requestCode, resultCode, data)
-                NewItemActivity.super.onActivityResult(requestCode, resultCode, data);
-                //veriicando se a chamada (requestCode) é referente a fornecida pela StartActivity com id de PHOTO_PICKER_REQUEST
-                if(requestCode == PHOTO_PICKER_REQUEST){
-                    //verificando se a chamada foi bem sucedida (resultCode)
-                    if(resultCode == Activity.RESULT_OK);{
-                        //obtendo o Uri da imagem escolhida e guardando em um atributo (photoSelected)
-                        photoSelected = data.getData();
-                        //obtendo o imageView
-                        ImageView imvfotoPreview = findViewById(R.id.imvfotoPreview);
-                        //setando o Uri
-                        imvfotoPreview.setImageURI(photoSelected);
-                    }
-                }
-            }
 
+                EditText etTitle = findViewById(R.id.etTitle);
+                String title = etTitle.getText().toString();
+                if (title.isEmpty()) {
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir um título", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                EditText etDesc = findViewById(R.id.etDesc);
+                String description = etDesc.getText().toString();
+                if (description.isEmpty()) {
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir uma descrição", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Intent i = new Intent();
+                i.setData(photoSelected);
+                i.putExtra("title", title);
+                i.putExtra("description", description);
+                setResult(Activity.RESULT_OK, i);
+                finish();
+            }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        //chamando a superclasse de onActivity com 3 parâmetros (requestCode, resultCode, data)
+        NewItemActivity.super.onActivityResult(requestCode, resultCode, data);
+        //veriicando se a chamada (requestCode) é referente a fornecida pela StartActivity com id de PHOTO_PICKER_REQUEST
+        if(requestCode == PHOTO_PICKER_REQUEST){
+            //verificando se a chamada foi bem sucedida (resultCode)
+            if(resultCode == Activity.RESULT_OK);{
+                //obtendo o Uri da imagem escolhida e guardando em um atributo (photoSelected)
+                photoSelected = data.getData();
+                //obtendo o imageView
+                ImageView imvfotoPreview = findViewById(R.id.imvfotoPreview);
+                //setando o Uri
+                imvfotoPreview.setImageURI(photoSelected);
+            }
+        }
+    }
+
+
 }
