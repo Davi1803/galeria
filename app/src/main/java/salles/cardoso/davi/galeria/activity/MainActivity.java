5728ec260db46b8b820e710f84cfb9bde0aa9500
20201tiimi0047 <sallescardosodavi@gmail.com>
2023-04-19 09:14:44 -0300

    fim sem icon

--- app/src/main/java/salles/cardoso/davi/galeria/activity/MainActivity.java
+++ app/src/main/java/salles/cardoso/davi/galeria/activity/MainActivity.java
@@ -2,6 +2,9 @@ package salles.cardoso.davi.galeria.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -14,20 +17,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 import salles.cardoso.davi.galeria.R;
+import salles.cardoso.davi.galeria.adapter.MyAdapter;
 import salles.cardoso.davi.galeria.model.MyItem;
 
 public class MainActivity extends AppCompatActivity {
 
     static int NEW_ITEM_REQUEST =1;
     List<MyItem> itens = new ArrayList<>();
+    MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //obtendo botão FAB
         FloatingActionButton fabAddItem = findViewById(R.id.fabAddNewItem);
+
+        //Obtendo um recycleView
+        RecyclerView rvItens = findViewById(R.id.rvItens);
+
+        //Criando um myAdapter
+        myAdapter = new MyAdapter(this,itens);
+        //Setando adapter no RecycleView
+        rvItens.setAdapter(myAdapter);
+
+        //Indicando ao RecycleView que não há variação de tamanho entre os itens da lista
+        rvItens.setHasFixedSize(true);
+
+        //Criando um gerenciador de layout do tipo linear
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        //Setando o gerenciador no RecycleView
+        rvItens.setLayoutManager(layoutManager);
+
+        //Criando um divisor de linhas para decorar a lista
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvItens.getContext(), DividerItemDecoration.VERTICAL);
+        //Setando o divisor no RecycleView
+        rvItens.addItemDecoration(dividerItemDecoration);
+
         //registrando um ouvidor de cliques
         fabAddItem.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -53,7 +81,10 @@ public class MainActivity extends AppCompatActivity {
 
                 //adicionando o item "myItem" em uma lista de itens (itens) já definida como atributo na classe
                 itens.add(myItem);
+                //Notificando o Adapter para que seja mostrado no RecycleView
+                myAdapter.notifyItemInserted(itens.size()-1);
             }
         }
     }
+
 }
--- app/src/main/java/salles/cardoso/davi/galeria/adapter/MyAdapter.java
+++ app/src/main/java/salles/cardoso/davi/galeria/adapter/MyAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,18 +29,37 @@ public class MyAdapter  extends RecyclerView.Adapter{
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        //Obtendo inflador de layouts
         LayoutInflater inflater = LayoutInflater.from(mainActivity);
+        //Utilizando o inflater para criar os elementos de interface e os guardando em uma View
         View v = inflater.inflate(R.layout.item_list,parent,false);
+        //A View é guardada dentro de um MyViewHolder e retornado na função
         return new MyViewHolder(v);
     }
+    @Override
+    //criando método para preencher a UI com os dados de um item, ele recebe os parâmetros holder e position
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
+        //Obtendo item que preencherá a UI
         MyItem myItem = itens.get(position);
 
+        //Pegando a View dentro da ViewHolder
         View v = holder.itemView;
 
-        ImageView imvfoto = v.findViewById(R.id.imvfoto);
-        imvfoto.setImageURI(myItem.title);
+        //Preenchendo a UI com os dados do Item, e os localizando com um findViewById
+        ImageView imvphoto = v.findViewById(R.id.imvphoto);
+        imvphoto.setImageURI(myItem.photo);
+
+        TextView tvTitle = v.findViewById(R.id.tvTitle);
+        tvTitle.setText(myItem.title);
+
+        TextView tvdesc = v.findViewById(R.id.tvdesc);
+        tvdesc.setText(myItem.description);
+
+    }
+    @Override
+    public int getItemCount(){
+        return itens.size();
     }
 
 }
